2ae2f894c16a48f674012a5a20e48eaddde7862f
jbanks85 <jbanks@seattleu.edu>
2020-01-25 14:44:11 -0800

    fizzbuzz

--- 2-fizzbuzz-fun/index.js
+++ 2-fizzbuzz-fun/index.js
@@ -10,17 +10,23 @@ const getargs = require("../modules/getargs")
 let input = getargs.getIntegerArg()
 
 
-function fizzbuzz(n){
-// TODO provide for tests for divisible for 3,5, and 15 to return desired output
+function fizzbuzz(n) {
+  if (n % 15 == 0) {
+    return `fizzbuzz`;
+  } else if (n % 3 == 0) {
+    return `fizz`;
+  } else if (n % 5 == 0) {
+    return `buzz`;
+  } else {
+    return ``
+  }
 }
 
-// TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 1-fizzbuzz <integer>");
 }
 else {
-  // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for (let i = 1; i <= input; i++) {
     console.log(`${i} ${fizzbuzz(i)}`);
   }
 }
